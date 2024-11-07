88a1056be2012998116f44bb308c93a8b4bdb2ab
Gerhard Jordan <mail@gerhardjordan.com>
2024-11-07 20:07:12 +1100

    Make flats index and show page available for everyone

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,7 @@
 class FlatsController < ApplicationController
 
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
