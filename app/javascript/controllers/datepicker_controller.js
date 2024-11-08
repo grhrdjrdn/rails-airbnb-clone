a8198af4bfc4561bded748b566715732883fd593
min liu <heather81472@gmail.com>
2024-11-09 07:34:33 +0800

    Disable bookeddates

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,8 +3,15 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static targets = ["startDate", "endDate", "bookedDates"]
+
+
   connect() {
     console.log("hello from datepicker controller")
-    flatpickr(this.element)
+    const bookedDatesArray = JSON.parse(this.bookedDatesTarget.dataset.bookedDates)
+    console.log(bookedDatesArray)
+    flatpickr(this.startDateTarget, { minDate: "today", disable: bookedDatesArray })
+    flatpickr(this.endDateTarget, { minDate: "today", disable: bookedDatesArray })
   }
+
 }
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,6 +3,8 @@ class Flat < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
 
+  has_many :bookings
+
   include PgSearch::Model
   pg_search_scope :search_by_name_and_description,
     against: [ :name, :description, :address ],
